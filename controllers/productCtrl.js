832e5e1fb9395b7d9751510d6790a8d932279a9d
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:27:24 +0530

    create product in product control

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -44,6 +44,7 @@ class APIfeatures {
         this.query = this.query.skip(skip).limit(limit)
         return this;
     }
+}
 
 
 const productCtrl = {
@@ -63,27 +64,27 @@ const productCtrl = {
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    },
+    createProduct: async(req, res) =>{
+        try {
+            const {product_id, title, price, description, content, images, category} = req.body;
+            if(!images) return res.status(400).json({msg: "No image upload"})
+
+            const product = await Products.findOne({product_id})
+            if(product)
+                return res.status(400).json({msg: "This product already exists."})
+
+            const newProduct = new Products({
+                product_id, title: title.toLowerCase(), price, description, content, images, category
+            })
+
+            await newProduct.save()
+            res.json({msg: "Created a product"})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
-//     createProduct: async(req, res) =>{
-//         try {
-//             const {product_id, title, price, description, content, images, category} = req.body;
-//             if(!images) return res.status(400).json({msg: "No image upload"})
-
-//             const product = await Products.findOne({product_id})
-//             if(product)
-//                 return res.status(400).json({msg: "This product already exists."})
-
-//             const newProduct = new Products({
-//                 product_id, title: title.toLowerCase(), price, description, content, images, category
-//             })
-
-//             await newProduct.save()
-//             res.json({msg: "Created a product"})
-
-//         } catch (err) {
-//             return res.status(500).json({msg: err.message})
-//         }
-//     },
 //     deleteProduct: async(req, res) =>{
 //         try {
 //             await Products.findByIdAndDelete(req.params.id)
