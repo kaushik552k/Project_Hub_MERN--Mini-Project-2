0341576f7d64d33ba724b79330ad48fbdce9d71c
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2021-04-23 23:50:57 +0530

    created upload.js for uploading media

---
+++ routes/upload.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+const cloudinary = require('cloudinary')
+const auth = require('../middleware/auth')
+const authAdmin = require('../middleware/authAdmin')
+const fs = require('fs')
+
+
