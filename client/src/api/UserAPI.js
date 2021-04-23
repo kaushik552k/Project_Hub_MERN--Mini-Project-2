2436dd15b20dea8d7443821f75d66cfce2192c6f
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-24 00:07:29 +0530

    add product to the cart

--- client/src/api/UserAPI.js
+++ client/src/api/UserAPI.js
@@ -30,26 +30,27 @@ function UserAPI(token) {
         }
     },[token])
 
+
+
+    const addCart = async (product) => {
+        if(!isLogged) return alert("Please login to continue buying")
+
+        const check = cart.every(item =>{
+            return item._id !== product._id
+        })
+
+        if(check){
+            setCart([...cart, {...product, quantity: 1}])
+
+            await axios.patch('/user/addcart', {cart: [...cart, {...product, quantity: 1}]}, {
+                headers: {Authorization: token}
+            })
+
+        }else{
+            alert("This product has been added to cart.")
+        }
+    }
 }
-
-//     const addCart = async (product) => {
-//         if(!isLogged) return alert("Please login to continue buying")
-
-//         const check = cart.every(item =>{
-//             return item._id !== product._id
-//         })
-
-//         if(check){
-//             setCart([...cart, {...product, quantity: 1}])
-
-//             await axios.patch('/user/addcart', {cart: [...cart, {...product, quantity: 1}]}, {
-//                 headers: {Authorization: token}
-//             })
-
-//         }else{
-//             alert("This product has been added to cart.")
-//         }
-//     }
 
 //     return {
 //         isLogged: [isLogged, setIsLogged],
