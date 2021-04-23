7a9d319bfef7c306512f1777cd2a8736cabae2e5
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2021-04-23 23:49:42 +0530

    created product router to create product

---
+++ routes/productRouter.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const productCtrl = require('../controllers/productCtrl')
+const auth = require('../middleware/auth')
+const authAdmin = require('../middleware/authAdmin')
+
+
+router.route('/products')
+    .get(productCtrl.getProducts)
+    .post(auth, authAdmin, productCtrl.createProduct)
+
+
+
+
+module.exports = router
