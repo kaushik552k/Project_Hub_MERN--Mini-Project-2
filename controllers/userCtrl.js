0f82247b1df29745a4c283f74f17bf6e2889ef0f
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:40:50 +0530

    getuser in user control

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -90,17 +90,17 @@ const userCtrl = {
             return res.status(500).json({msg: err.message})
         }
         
-    }
-//     getUser: async (req, res) =>{
-//         try {
-//             const user = await Users.findById(req.user.id).select('-password')
-//             if(!user) return res.status(400).json({msg: "User does not exist."})
+    },
+    getUser: async (req, res) =>{
+        try {
+            const user = await Users.findById(req.user.id).select('-password')
+            if(!user) return res.status(400).json({msg: "User does not exist."})
 
-//             res.json(user)
-//         } catch (err) {
-//             return res.status(500).json({msg: err.message})
-//         }
-//     },
+            res.json(user)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
 //     addCart: async (req, res) =>{
 //         try {
 //             const user = await Users.findById(req.user.id)
