6429c80c16e92d5066b610df670002ad309bfd60
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:40:17 +0530

    creating refresh token in user control

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -72,25 +72,25 @@ const userCtrl = {
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
-    }
-//     refreshToken: (req, res) =>{
-//         try {
-//             const rf_token = req.cookies.refreshtoken;
-//             if(!rf_token) return res.status(400).json({msg: "Please Login or Register"})
+    },
+    refreshToken: (req, res) =>{
+        try {
+            const rf_token = req.cookies.refreshtoken;
+            if(!rf_token) return res.status(400).json({msg: "Please Login or Register"})
 
-//             jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) =>{
-//                 if(err) return res.status(400).json({msg: "Please Login or Register"})
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) =>{
+                if(err) return res.status(400).json({msg: "Please Login or Register"})
 
-//                 const accesstoken = createAccessToken({id: user.id})
+                const accesstoken = createAccessToken({id: user.id})
 
-//                 res.json({accesstoken})
-//             })
+                res.json({accesstoken})
+            })
 
-//         } catch (err) {
-//             return res.status(500).json({msg: err.message})
-//         }
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
         
-//     },
+    }
 //     getUser: async (req, res) =>{
 //         try {
 //             const user = await Users.findById(req.user.id).select('-password')
