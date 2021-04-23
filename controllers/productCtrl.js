3b8c3ced810f4530ca87363c076eb845fc2b0310
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:26:09 +0530

    pagination

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -37,14 +37,14 @@ class APIfeatures {
         return this;
     }
 
-//     paginating(){
-//         const page = this.queryString.page * 1 || 1
-//         const limit = this.queryString.limit * 1 || 9
-//         const skip = (page - 1) * limit;
-//         this.query = this.query.skip(skip).limit(limit)
-//         return this;
-//     }
-// }
+    paginating(){
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 9
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit)
+        return this;
+    }
+
 
 // const productCtrl = {
 //     getProducts: async(req, res) =>{
