2eb8995a511d887381cdf0ae37e11c7cda7d9f8e
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:39:01 +0530

    login in user control

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -38,33 +38,33 @@ const userCtrl = {
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    },
+    login: async (req, res) =>{
+        try {
+            const {email, password} = req.body;
+
+            const user = await Users.findOne({email})
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({msg: "Incorrect password."})
+
+            // If login success , create access token and refresh token
+            const accesstoken = createAccessToken({id: user._id})
+            const refreshtoken = createRefreshToken({id: user._id})
+
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                maxAge: 7*24*60*60*1000 // 7d
+            })
+
+            res.json({accesstoken})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
-//     login: async (req, res) =>{
-//         try {
-//             const {email, password} = req.body;
-
-//             const user = await Users.findOne({email})
-//             if(!user) return res.status(400).json({msg: "User does not exist."})
-
-//             const isMatch = await bcrypt.compare(password, user.password)
-//             if(!isMatch) return res.status(400).json({msg: "Incorrect password."})
-
-//             // If login success , create access token and refresh token
-//             const accesstoken = createAccessToken({id: user._id})
-//             const refreshtoken = createRefreshToken({id: user._id})
-
-//             res.cookie('refreshtoken', refreshtoken, {
-//                 httpOnly: true,
-//                 path: '/user/refresh_token',
-//                 maxAge: 7*24*60*60*1000 // 7d
-//             })
-
-//             res.json({accesstoken})
-
-//         } catch (err) {
-//             return res.status(500).json({msg: err.message})
-//         }
-//     },
 //     logout: async (req, res) =>{
 //         try {
 //             res.clearCookie('refreshtoken', {path: '/user/refresh_token'})
