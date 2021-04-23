09bbd73b1576dc6bdeba27d5c4fdd385f753d18f
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:37:24 +0530

    register user

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -3,42 +3,42 @@ const Payments = require('../models/paymentModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-// const userCtrl = {
-//     register: async (req, res) =>{
-//         try {
-//             const {name, email, password} = req.body;
+const userCtrl = {
+    register: async (req, res) =>{
+        try {
+            const {name, email, password} = req.body;
 
-//             const user = await Users.findOne({email})
-//             if(user) return res.status(400).json({msg: "The email already exists."})
+            const user = await Users.findOne({email})
+            if(user) return res.status(400).json({msg: "The email already exists."})
 
-//             if(password.length < 6) 
-//                 return res.status(400).json({msg: "Password is at least 6 characters long."})
+            if(password.length < 6) 
+                return res.status(400).json({msg: "Password is at least 6 characters long."})
 
-//             // Password Encryption
-//             const passwordHash = await bcrypt.hash(password, 10)
-//             const newUser = new Users({
-//                 name, email, password: passwordHash
-//             })
+            // Password Encryption
+            const passwordHash = await bcrypt.hash(password, 10)
+            const newUser = new Users({
+                name, email, password: passwordHash
+            })
 
-//             // Save mongodb
-//             await newUser.save()
+            // Save mongodb
+            await newUser.save()
 
-//             // Then create jsonwebtoken to authentication
-//             const accesstoken = createAccessToken({id: newUser._id})
-//             const refreshtoken = createRefreshToken({id: newUser._id})
+            // Then create jsonwebtoken to authentication
+            const accesstoken = createAccessToken({id: newUser._id})
+            const refreshtoken = createRefreshToken({id: newUser._id})
 
-//             res.cookie('refreshtoken', refreshtoken, {
-//                 httpOnly: true,
-//                 path: '/user/refresh_token',
-//                 maxAge: 7*24*60*60*1000 // 7d
-//             })
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                maxAge: 7*24*60*60*1000 // 7d
+            })
 
-//             res.json({accesstoken})
+            res.json({accesstoken})
 
-//         } catch (err) {
-//             return res.status(500).json({msg: err.message})
-//         }
-//     },
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    }
 //     login: async (req, res) =>{
 //         try {
 //             const {email, password} = req.body;
@@ -122,9 +122,9 @@ const jwt = require('jsonwebtoken')
 //             res.json(history)
 //         } catch (err) {
 //             return res.status(500).json({msg: err.message})
-//         }
-//     }
-//  }
+    //     }
+    // }
+ }
 
 
 // const createAccessToken = (user) =>{
