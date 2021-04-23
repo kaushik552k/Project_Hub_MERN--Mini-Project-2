26a03570abff8f781b8bccf2285f4971c861eadf
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2021-04-23 23:53:14 +0530

    code for uploading access for admin only

--- routes/upload.js
+++ routes/upload.js
@@ -12,4 +12,36 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 })
 
+// Upload image only admin can use
+router.post('/upload',auth , authAdmin, (req, res) =>{
+    try {
+        if(!req.files || Object.keys(req.files).length === 0)
+            return res.status(400).json({msg: 'No files were uploaded.'})
+        
+        const file = req.files.file;
+        if(file.size > 1024*1024) {
+            removeTmp(file.tempFilePath)
+            return res.status(400).json({msg: "Size too large"})
+        }
+
+        if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+            removeTmp(file.tempFilePath)
+            return res.status(400).json({msg: "File format is incorrect."})
+        }
+
+        cloudinary.v2.uploader.upload(file.tempFilePath, {folder: "test"}, async(err, result)=>{
+            if(err) throw err;
+
+            removeTmp(file.tempFilePath)
+
+            res.json({public_id: result.public_id, url: result.secure_url})
+        })
+
+
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+})
+
+
 module.exports = router
