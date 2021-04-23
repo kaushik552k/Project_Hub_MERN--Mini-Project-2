d5d048538cbd6d9dbfe6117baaeb256b60b8e0f3
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2021-04-23 23:34:58 +0530

    created payment model

---
+++ models/paymentModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+
+const paymentSchema = new mongoose.Schema({
+    user_id: {
+        type: String,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    paymentID:{
+        type: String,
+        required: true
+    },
+    
+})
+
+
+module.exports = mongoose.model("Payments", paymentSchema)
