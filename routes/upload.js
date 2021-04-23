c6647c158f9974072be008f193391a1beac3b808
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2021-04-23 23:55:49 +0530

    code to delete image by admin only

--- routes/upload.js
+++ routes/upload.js
@@ -44,4 +44,28 @@ router.post('/upload',auth , authAdmin, (req, res) =>{
 })
 
 
+// Delete image only admin can use
+router.post('/destroy',auth , authAdmin, (req, res) =>{
+    try {
+        const {public_id} = req.body;
+        if(!public_id) return res.status(400).json({msg: 'No images Selected'})
+
+        cloudinary.v2.uploader.destroy(public_id, async(err, result) =>{
+            if(err) throw err;
+
+            res.json({msg: "Deleted Image"})
+        })
+
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+    
+})
+
+
+const removeTmp = (path) =>{
+    fs.unlink(path, err=>{
+        if(err) throw err;
+    })
+}
 module.exports = router
