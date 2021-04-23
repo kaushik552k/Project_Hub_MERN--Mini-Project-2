3184d4238599729314c8a4fb27131d9c6d590c20
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:41:43 +0530

    history in user control

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -114,16 +114,16 @@ const userCtrl = {
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
-    }
-//     history: async(req, res) =>{
-//         try {
-//             const history = await Payments.find({user_id: req.user.id})
+    },
+    history: async(req, res) =>{
+        try {
+            const history = await Payments.find({user_id: req.user.id})
 
-//             res.json(history)
-//         } catch (err) {
-//             return res.status(500).json({msg: err.message})
-    //     }
-    // }
+            res.json(history)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    }
  }
 
 
