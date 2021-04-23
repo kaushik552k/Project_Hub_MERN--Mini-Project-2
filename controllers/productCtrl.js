46fd36561b66f88a3f7cd8b6fab8ddc7735fe2e5
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:29:25 +0530

    update product in product control

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -92,20 +92,22 @@ const productCtrl = {
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    },
+    updateProduct: async(req, res) =>{
+        try {
+            const {title, price, description, content, images, category} = req.body;
+            if(!images) return res.status(400).json({msg: "No image upload"})
+
+            await Products.findOneAndUpdate({_id: req.params.id}, {
+                title: title.toLowerCase(), price, description, content, images, category
+            })
+
+            res.json({msg: "Updated a Product"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
-//     updateProduct: async(req, res) =>{
-//         try {
-//             const {title, price, description, content, images, category} = req.body;
-//             if(!images) return res.status(400).json({msg: "No image upload"})
-
-//             await Products.findOneAndUpdate({_id: req.params.id}, {
-//                 title: title.toLowerCase(), price, description, content, images, category
-//             })
-
-//             res.json({msg: "Updated a Product"})
-//         } catch (err) {
-//             return res.status(500).json({msg: err.message})
-         }
+}
     
 
 
