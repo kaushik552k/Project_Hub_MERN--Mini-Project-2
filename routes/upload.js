ac56dbf880fd744efb72a44bb636ec90cb9b1a7c
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2021-04-23 23:51:39 +0530

    code to uploade image on cloudinary

--- routes/upload.js
+++ routes/upload.js
@@ -5,3 +5,11 @@ const authAdmin = require('../middleware/authAdmin')
 const fs = require('fs')
 
 
+// we will upload image on cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+})
+
+module.exports = router
