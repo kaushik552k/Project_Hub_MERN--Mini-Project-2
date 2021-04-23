1fc64e1af1ad643fa2ee7f484a4575055452f21e
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:39:35 +0530

    creating logout in user control

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -64,15 +64,15 @@ const userCtrl = {
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    },
+    logout: async (req, res) =>{
+        try {
+            res.clearCookie('refreshtoken', {path: '/user/refresh_token'})
+            return res.json({msg: "Logged out"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
-//     logout: async (req, res) =>{
-//         try {
-//             res.clearCookie('refreshtoken', {path: '/user/refresh_token'})
-//             return res.json({msg: "Logged out"})
-//         } catch (err) {
-//             return res.status(500).json({msg: err.message})
-//         }
-//     },
 //     refreshToken: (req, res) =>{
 //         try {
 //             const rf_token = req.cookies.refreshtoken;
