688b963f3ed86a0d6e4925e4e2bcb3f778b154a7
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2021-04-23 23:45:27 +0530

    added category router

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -4,13 +4,3 @@ const auth = require('../middleware/auth')
 const authAdmin = require('../middleware/authAdmin')
 
 
-router.route('/category')
-    .get(categoryCtrl.getCategories)
-    .post(auth, authAdmin, categoryCtrl.createCategory)
-
-router.route('/category/:id')
-    .delete(auth, authAdmin, categoryCtrl.deleteCategory)
-    .put(auth, authAdmin, categoryCtrl.updateCategory)
-
-
-module.exports = router
