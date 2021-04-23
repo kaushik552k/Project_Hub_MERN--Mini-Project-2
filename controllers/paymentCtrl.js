c422944b9879db50d19844b84b9b5f54b4fe3d90
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:21:59 +0530

    create payement in payment control

--- controllers/paymentCtrl.js
+++ controllers/paymentCtrl.js
@@ -11,39 +11,38 @@ const paymentCtrl = {
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
-//     },
-//     createPayment: async(req, res) => {
-//         try {
-//             const user = await Users.findById(req.user.id).select('name email')
-//             if(!user) return res.status(400).json({msg: "User does not exist."})
+    },
+    createPayment: async(req, res) => {
+        try {
+            const user = await Users.findById(req.user.id).select('name email')
+            if(!user) return res.status(400).json({msg: "User does not exist."})
 
-//             const {cart, paymentID, address} = req.body;
+            const {cart, paymentID, address} = req.body;
 
-//             const {_id, name, email} = user;
+            const {_id, name, email} = user;
 
-//             const newPayment = new Payments({
-//                 user_id: _id, name, email, cart, paymentID, address
-//             })
+            const newPayment = new Payments({
+                user_id: _id, name, email, cart, paymentID, address
+            })
 
-//             cart.filter(item => {
-//                 return sold(item._id, item.quantity, item.sold)
-//             })
+            cart.filter(item => {
+                return sold(item._id, item.quantity, item.sold)
+            })
 
             
-//             await newPayment.save()
-//             res.json({msg: "Payment Succes!"})
+            await newPayment.save()
+            res.json({msg: "Payment Succes!"})
             
-//         } catch (err) {
-//             return res.status(500).json({msg: err.message})
-//         }
-//     }
-// }
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    }
+}
 
 // const sold = async (id, quantity, oldSold) =>{
 //     await Products.findOneAndUpdate({_id: id}, {
 //         sold: quantity + oldSold
 //     })
-}
-}
+
 
 module.exports = paymentCtrl
