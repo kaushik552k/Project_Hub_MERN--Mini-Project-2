569ef1713d340a950ca39eb2ead62ef5c22979fb
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-04-23 23:38:30 +0530

    creating refresh token

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -130,9 +130,9 @@ const userCtrl = {
 const createAccessToken = (user) =>{
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '11m'})
 }
-// const createRefreshToken = (user) =>{
-//     return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
-// }
+const createRefreshToken = (user) =>{
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
+}
 
 module.exports = userCtrl
 
