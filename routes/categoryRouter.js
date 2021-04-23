53988fa843dbc48a70309bed1b8618e55599c4c8
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2021-04-23 23:46:39 +0530

    method to create category

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -4,3 +4,13 @@ const auth = require('../middleware/auth')
 const authAdmin = require('../middleware/authAdmin')
 
 
+router.route('/category')
+    .get(categoryCtrl.getCategories)
+    .post(auth, authAdmin, categoryCtrl.createCategory)
+
+router.route('/category/:id')
+    .delete(auth, authAdmin, categoryCtrl.deleteCategory)
+    .put(auth, authAdmin, categoryCtrl.updateCategory)
+
+
+module.exports = router
